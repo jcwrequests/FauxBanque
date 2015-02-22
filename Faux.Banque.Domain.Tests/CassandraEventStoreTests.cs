d9123537a0460e3d622bce3518590c956f93a508
jcwrequests@comcast.net <jcwrequests@comcast.net>
2015-02-21 21:49:51 -0500

    Added new tests

--- Faux.Banque.Domain.Tests/CassandraEventStoreTests.cs
+++ Faux.Banque.Domain.Tests/CassandraEventStoreTests.cs
@@ -8,6 +8,7 @@ namespace Faux.Banque.Domain.Tests
     [TestClass]
     public class CassandraEventStoreTests
     {
+        [TestCategory("CASSANDRA_EVENT_STORE")]
         [TestMethod]
         public void CREATE_SESSION()
         {
@@ -22,6 +23,7 @@ namespace Faux.Banque.Domain.Tests
 
         }
 
+        [TestCategory("CASSANDRA_EVENT_STORE")]
         [TestMethod]
         public void CREATE_STORE()
         {
@@ -38,6 +40,7 @@ namespace Faux.Banque.Domain.Tests
             cluster.Dispose();
             session.Dispose();
         }
+        [TestCategory("CASSANDRA_EVENT_STORE")]
         [TestMethod]
         public void INITIALIZE_STORE()
         {
@@ -54,6 +57,7 @@ namespace Faux.Banque.Domain.Tests
             cluster.Dispose();
             session.Dispose();
         }
+        [TestCategory("CASSANDRA_EVENT_STORE")]
         [TestMethod]
         public void GET_CURRENT_VERSION()
         {
@@ -81,6 +85,7 @@ namespace Faux.Banque.Domain.Tests
             cluster.Dispose();
             session.Dispose();
         }
+        [TestCategory("CASSANDRA_EVENT_STORE")]
         [TestMethod]
         public void READ_RECORDS_FROM_GIVEN_POINT_IN_TIME_MOVING_FORWARD()
         {
@@ -93,6 +98,34 @@ namespace Faux.Banque.Domain.Tests
             CassandraEventStore cassandraES = new CassandraEventStore(session);
 
             var records = cassandraES.ReadRecords(DateTimeOffset.MinValue, int.MaxValue);
+
+            try
+            {
+                records.Wait();
+            }
+            catch (Exception e)
+            {
+
+                GetExcepton(e);
+            }
+
+
+            cluster.Dispose();
+            session.Dispose();
+        }
+        [TestCategory("CASSANDRA_EVENT_STORE")]
+        [TestMethod]
+        public void READ_ALL_RECORDS_FOR_GIVEN_KEY()
+        {
+            var cluster = Cluster.Builder()
+                 .AddContactPoints("localhost")
+                     .Build();
+
+            ISession session = cluster.Connect("EventStore");
+
+            CassandraEventStore cassandraES = new CassandraEventStore(session);
+
+            var records = cassandraES.ReadRecords("account1",0, int.MaxValue);
 
             try
             {
